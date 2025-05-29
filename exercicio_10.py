dab3fcf73c1d1a0240fef7573c97a845daaad4c8
Yuri3358 <yurialmeida917@gmail.com>
2025-05-29 19:39:45 -0300

    fix(return): missing figure return

--- exercicio_10.py
+++ exercicio_10.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 
 def plot_histograma(valores: list[float], bins: int = 10) -> Figure:
-    plt.figure()
+    hist_figure = plt.figure()
     plt.hist(valores, bins)
     plt.xlabel("Valores da Lista")
     plt.ylabel("Número de Ocorrências")
-    plt.show()
+    return hist_figure
