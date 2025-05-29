cf5ff9fc510c40ebb26fe431043f564ed6657cc3
Yuri3358 <turialmeida917@gmail.com>
2025-05-29 18:50:23 -0300

    fix: dataframe describe and manual solution don't match

--- exercicio_08.py
+++ exercicio_08.py
@@ -1,7 +1,7 @@
 import pandas
 def cria_resumo(df: pandas.DataFrame, coluna: str) -> pandas.DataFrame:
     data = df[coluna]
-    manual_describe_data = pandas.DataFrame.from_dict({
+    manual_describe_data = pandas.Series({
         "count": data.count(),
         "mean": data.mean(),
         "std": data.std(),
@@ -10,5 +10,5 @@ def cria_resumo(df: pandas.DataFrame, coluna: str) -> pandas.DataFrame:
         "50%": data.quantile(0.5), 
         "75%": data.quantile(0.75),
         "max": data.max()
-    }, orient="index")
-    return manual_describe_data
+    })
+    return manual_describe_data.to_frame(coluna)
